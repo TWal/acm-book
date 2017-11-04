ef99618cb16a5aa8ae12c1824042ae2c179e3e95
Lucas-84 <lucas.pesenti2@gmail.com>
2017-11-04 21:38:27 +0100

    Shorten full LIS

--- src/misc/lis_full.cpp
+++ src/misc/lis_full.cpp
@@ -1,18 +1,15 @@
 // Returns the indices of the longest increasing subsequence of v
 vi lis(const vi& v) {
     lli n = v.size(), c = 0;
-    vi b(n), p(n), ans;
-    for (int i = 0; i < n; ++i) {
+    vi b(n), p(n);
+    FOR(i, n) {
         // Longest nondecreasing subsequence: replace with upper_bound
         p[i] = lower_bound(b.begin(), b.begin() + c, v[i]) - b.begin() + 1;
         b[p[i] - 1] = v[i];
         c = max(c, p[i]);
     }
-    for (int i = n - 1; i >= 0; --i) if (p[i] == c) {
-        ans.pb(i);
-        c--;
-    }
-    reverse(ans.begin(), ans.end());
+    // Return c here if we just need the length 
+    vi ans(c);
+    FORD(i, 0, n) if (p[i] == c) ans[--c] = i;
     return ans;
 }
-
