a35bab8324d177ae99079f59e48fa070ad16418c
Théophile Wallez <theophile@wallez.net>
2017-11-08 11:31:21 +0100

    Shorten implementation of walsh-hadamard and subset-sum

--- src/math/subset_sum.cpp
+++ src/math/subset_sum.cpp
@@ -2,13 +2,11 @@
 //Fourier transform for convolutions like FOR(i, n) FOR(j, n) c[i|j] += a[i]*b[j]
 void subsetSum(vi& v, bool inverse) {
     assert((v.size() & (v.size()-1)) == 0); //check that v.size() is a power of 2
-    lli z = v.size() >> 1;
-    while(z > 0) {
+    for(lli z = v.size() >> 1; z > 0; z >>= 1) {
         FOR(i, v.size()) {
             if(i & z) continue;
             v[i|z] += (inverse ? -v[i] : v[i]);
         }
-        z >>= 1;
     }
 }
 
--- src/math/walsh_hadamard.cpp
+++ src/math/walsh_hadamard.cpp
@@ -3,14 +3,12 @@
 //Fourier transform for convolutions like FOR(i, n) FOR(j, n) c[i^j] += a[i]*b[j]
 void hadamard(vi& v, bool inverse) {
     assert((v.size() & (v.size()-1)) == 0); //check that v.size() is a power of 2
-    lli z = v.size() >> 1;
-    while(z > 0) {
+    for(lli z = v.size() >> 1; z > 0; z >>= 1) {
         FOR(i, v.size()) {
             if(i & z) continue;
             lli a = v[i], b = v[i|z];
             v[i] = a+b, v[i|z] = a-b;
         }
-        z >>= 1;
     }
     if(inverse) {
         FOR(i, v.size()) {
