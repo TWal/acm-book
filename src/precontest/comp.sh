371739a66f2550c2566efecf281002e888a720ee
TWal <theophile@wallez.net>
2020-01-24 16:44:48 +0100

    Update

--- src/precontest/comp.sh
+++ src/precontest/comp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if g++ -DDEBUG -std=c++14 -g -Wall -Wextra $1.cpp -o $1; then
+if g++ -DDEBUG -D_GLIBCXX_DEBUG -std=c++14 -g -Wall -Wextra $1.cpp -o $1; then
     for i in $1.in*; do
         echo $i
-        time ./$1 < $i
+        valgrind -q ./$1 < $i
     done
 fi
 
